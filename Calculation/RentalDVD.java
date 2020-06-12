5b60387c6e7dc46c9266ec6bbfc00c0946b69d3c
SEONMIN99 <seonmin99@naver.com>
2020-06-12 10:57:04 +0900

    Initial sharing of project

---
+++ Calculation/RentalDVD.java
@@ -0,0 +1,32 @@
+package Calculation;
+
+
+/**
+ * RentalDVD 가격 계산 클래스 
+ * 
+ * @author (2018315056 우메모토 세이야, 2015225129 이재근, 2018315021 방대호) 
+ * @version (2020/06/10)
+ */
+public class RentalDVD
+{
+    /**
+     * calcRentalFee: 나이, 대여요금, 대여 시작일로부터 경과된 시간에 따라 할인된 가격을 출력해주는 메소드
+     * 
+     * Parameter: age (0이상 150이하의 정수값), fee (음수가 아닌 정수값), period 대여 시작일로부터 경과된 시간(음수가 아닌 정수값)
+     * return: 할인 적용 후 가격
+     */
+    public static int calcRentalFee(int age, int fee, int period) {
+        boolean isOldWork = false;
+        if ((age > 150) || (age <0 ) || (fee <0) || (period <0))
+            fee = -1;
+        if (6 <= period)
+            isOldWork = true;
+        if (isOldWork)
+            fee *= 0.5;
+        else if ((65 <= age) && (age <= 150))
+            fee *= 0.8;
+        else if ((0 <= age) && (age <= 18))
+            fee *= 0.9;
+        return fee;
+    }
+}
---
+++ Main/MyApp.java
@@ -0,0 +1,15 @@
+package Main;
+
+
+/**
+ * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * 
+ * @author (2018315034 정철우, 2018315038 이혜인, 2018315040 김선민) 
+ * @version (2020.06.12)
+ */
+public class MyApp
+{
+    public static void main(String[] args){
+    
+    }
+}

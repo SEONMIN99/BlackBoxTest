e3513f94eb8115c0559faee21680fdf3b0f086d1
Hye-inLee <asd047017@gmail.com>
2020-06-12 11:17:39 +0900

    println수정

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -11,9 +11,9 @@ public class MyApp
 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("나이를 입력하세요: " );
-        System.out.print("대여요금을 입력하세요: ");
-        System.out.print("대여 시작일로부터 경과된 시간을 입력하세요: ");
+        System.out.println("나이를 입력하세요: " );
+        System.out.println("대여요금을 입력하세요: ");
+        System.out.println("대여 시작일로부터 경과된 시간을 입력하세요: ");
         int age = sc.nextInt();
         int fee = sc.nextInt();
         int period = sc.nextInt();
